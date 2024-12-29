c0531130784ae5d714d730eb436fedd95d703844
inna_boby <pooh_08@mail.ru>
2024-12-29 23:49:28 +0300

    streamlit app

---
+++ checkpoint4/stapp_page5.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import asyncio
+from client import invoke
+
+def page5():
+    st.title('Запрос')
+    query=st.text_input('Введите запрос')
+    if st.button('Отправить'):
+        async def main():
+            return await invoke('http://localhost:8000/api/v1/invoke', query)
+
+        st.write(asyncio.run(main()))
