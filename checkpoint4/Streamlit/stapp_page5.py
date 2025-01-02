59789d064f8f7d3061b89a81e2e6ec977a4f5516
ShermanKsenia <71763287+ShermanKsenia@users.noreply.github.com>
2025-01-02 18:27:41 +0300

    Add files via upload

--- checkpoint4/Streamlit/stapp_page5.py
+++ checkpoint4/Streamlit/stapp_page5.py
@@ -1,10 +1,21 @@
 import streamlit as st
 import asyncio
 from Streamlit.client import invoke
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    filename='./logs/streamlit.log',
+    filemode='a',
+)
+logger = logging.getLogger('./logs/streamlit.log')
 
 def page5():
     st.title('Запрос')
     query=st.text_input('Введите запрос')
+    logger.info(f"Пользователь ввел запрос: {query}")
     if st.button('Отправить'):
         async def main():
             return await invoke('http://localhost:8000/api/v1/invoke', query)
